27c15f431d011744aaeda89bf031ffeb4b05fdf1
LAASYA LATA <71040750+laasya2005@users.noreply.github.com>
2021-09-16 05:07:29 +0530

    datatypes and variables

---
+++ dataType.java
@@ -0,0 +1,17 @@
+package datatypes;
+
+public class dataType {
+
+	public static void main(String[] args) {
+		byte x = 2;
+		int age =10;
+		float rateOfInterest= 2.3f;
+		double rate= 3.142851;
+		System.out.println(rateOfInterest);
+		boolean isLearningJavaCool= true;
+		char myCharacter='@';
+		
+
+	}
+
+}
